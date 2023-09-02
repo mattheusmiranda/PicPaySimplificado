c7a13f427d9140895647b16d0a00c4735ba2ae39
matheus <matheusrod59@gmail.com>
2023-09-02 11:51:42 -0300

    Correção de mapeamento na classe Transaction, implementado get's/set's nas classes de domain, criado validações em UserService.

--- src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -9,26 +9,87 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity(name = "transactions")
 @Table(name = "transactions")
-@Getter
-@Setter
-@AllArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class Transaction {
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+	    private BigDecimal amount;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private BigDecimal amount;
+	    @ManyToOne
+	    @JoinColumn(name = "sender_id")
+	    private User sender;
 
-    @ManyToMany
-    @JoinColumn(name = "sender_id")
-    private User sender;
-    @ManyToMany
-    @JoinColumn(name = "receiver_id")
-    private User receiver;
+	    @ManyToOne
+	    @JoinColumn(name = "receiver_id")
+	    private User receiver;
 
-    private LocalDateTime timesTamp;
+	    private LocalDateTime timesTamp;
+
+    public Transaction() {
+    }
+
+    public Transaction(Long id, BigDecimal amount, User sender, User receiver, LocalDateTime timesTamp) {
+        this.id = id;
+        this.amount = amount;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.timesTamp = timesTamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Objects.equals(id, that.id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public LocalDateTime getTimesTamp() {
+        return timesTamp;
+    }
+
+    public void setTimesTamp(LocalDateTime timesTamp) {
+        this.timesTamp = timesTamp;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,19 +1,14 @@
 package com.picpaysimplificado.domain.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
+@Data
 @Entity(name = "users")
 @Table(name = "users")
-@Getter
-@Setter
-@AllArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class User {
 
     @Id
@@ -32,4 +27,95 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public User(Long id, String firstName, String lastName, String document, String email, String password, BigDecimal balance, UserType userType) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.userType = userType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
 }
